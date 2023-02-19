1b6f898b33e516d7691977dce2096c1298305748
jrbigmon <juniorterriaga0@gmail.com>
2023-02-19 16:00:19 -0300

    feat: add logs module in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LogsModule],
 })
 export class AppModule {}
